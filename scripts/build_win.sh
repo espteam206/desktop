d661357ef05271028b847feae6a948810bca2edd
cucumberbolts <cucumberbolts@gmail.com>
2026-03-18 00:40:00 -0400

    Fix dll not found issue

--- scripts/build_win.sh
+++ scripts/build_win.sh
@@ -4,5 +4,5 @@ set -xe
 
 cmake -B build_win -DCMAKE_TOOLCHAIN_FILE=cmake/toolchain-mingw64.cmake
 cmake --build build_win
-export WINEPATH="/usr/x86_64-w64-mingw32/sys-root/mingw/bin/"
+# export WINEPATH="/usr/x86_64-w64-mingw32/sys-root/mingw/bin/"
 export WINEDEBUG="fixme-all"
